dac5ad25273395863c61e8501534a154efc23ec5
Xare123 <57245242+Xare123@users.noreply.github.com>
2026-09-03 02:31:22 -0700

    Repair stale Flutter web model exports

--- lib/database/models.dart
+++ lib/database/models.dart
@@ -1,29 +1,29 @@
 export 'package:bluebubbles/database/io/attachment.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/attachment.dart';
-export 'package:bluebubbles/database/io/chat.dart' if (dart.library.html) 'package:bluebubbles/models/html/chat.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/attachment.dart';
+export 'package:bluebubbles/database/io/chat.dart' if (dart.library.html) 'package:bluebubbles/database/html/chat.dart';
 export 'package:bluebubbles/database/io/cloud_sync_records.dart'
     if (dart.library.html) 'package:bluebubbles/database/html/cloud_sync_records.dart';
 export 'package:bluebubbles/database/io/contact.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/contact.dart';
-export 'package:media_kit/media_kit.dart' if (dart.library.html) 'package:bluebubbles/models/html/media_kit.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/contact.dart';
+export 'package:media_kit/media_kit.dart' if (dart.library.html) 'package:bluebubbles/database/html/media_kit.dart';
 export 'package:media_kit_video/media_kit_video.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/media_kit.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/media_kit.dart';
 export 'package:bluebubbles/database/io/fcm_data.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/fcm_data.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/fcm_data.dart';
 export 'package:bluebubbles/database/io/handle.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/handle.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/handle.dart';
 export 'package:bluebubbles/database/io/launch_at_startup.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/launch_at_startup.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/launch_at_startup.dart';
 export 'package:bluebubbles/database/io/message.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/message.dart';
-export 'package:bluebubbles/objectbox.g.dart' if (dart.library.html) 'package:bluebubbles/models/html/objectbox.dart'
+    if (dart.library.html) 'package:bluebubbles/database/html/message.dart';
+export 'package:bluebubbles/objectbox.g.dart' if (dart.library.html) 'package:bluebubbles/database/html/objectbox.dart'
     hide Version;
-export 'package:bluebubbles/database/io/theme.dart' if (dart.library.html) 'package:bluebubbles/models/html/theme.dart';
+export 'package:bluebubbles/database/io/theme.dart' if (dart.library.html) 'package:bluebubbles/database/html/theme.dart';
 export 'package:bluebubbles/database/io/theme_entry.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/theme_entry.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/theme_entry.dart';
 export 'package:bluebubbles/database/io/theme_object.dart'
-    if (dart.library.html) 'package:bluebubbles/models/html/theme_object.dart';
-export 'package:bluebubbles/database/io/giphy.dart' if (dart.library.html) 'package:bluebubbles/models/html/giphy.dart';
+    if (dart.library.html) 'package:bluebubbles/database/html/theme_object.dart';
+export 'package:bluebubbles/database/io/giphy.dart';
 export 'package:bluebubbles/database/global/platform_file.dart';
 export 'package:bluebubbles/database/global/settings.dart';
 export 'package:bluebubbles/database/global/attributed_body.dart';
